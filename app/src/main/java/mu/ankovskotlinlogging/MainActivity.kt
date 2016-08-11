3b0287925e71cb69be626d526a45e7e192f79736
oshai <ohadshai@gmail.com>
2016-08-12 00:24:15 +0300

    added measure of logging time

--- app/src/main/java/mu/ankovskotlinlogging/MainActivity.kt
+++ app/src/main/java/mu/ankovskotlinlogging/MainActivity.kt
@@ -3,19 +3,51 @@ package mu.ankovskotlinlogging
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.common.base.Stopwatch
 import mu.KLogging
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.v(TAG, "This is logging of - android.util.Log logging");
-        logger.info { "This is logging of - kotlin-logging" }
-        info("This is logging of - AnkoLogger")
-        info{"This is logging of - AnkoLogger2"}
+        test("android-logging", {Log.v(TAG, "This is logging of - android.util.Log logging")});
+        test("android-logging", {Log.v(TAG, "This is logging of - android.util.Log logging")});
+        test("android-logging", {Log.v(TAG, "This is logging of - android.util.Log logging")});
+        test("android-logging", {Log.v(TAG, "This is logging of - android.util.Log logging")});
+        test("android-logging", {Log.v(TAG, "This is logging of - android.util.Log logging")});
+        test("kotlin-logging", {logger.info ("This is logging of - kotlin-logging" )});
+        test("kotlin-logging", {logger.info ("This is logging of - kotlin-logging" )});
+        test("kotlin-logging", {logger.info ("This is logging of - kotlin-logging" )});
+        test("kotlin-logging", {logger.info ("This is logging of - kotlin-logging" )});
+        test("kotlin-logging", {logger.info ("This is logging of - kotlin-logging" )});
+        test("kotlin-logging-lazy", {logger.info {"This is logging of - kotlin-logging"}});
+        test("kotlin-logging-lazy", {logger.info {"This is logging of - kotlin-logging"}});
+        test("kotlin-logging-lazy", {logger.info {"This is logging of - kotlin-logging"}});
+        test("kotlin-logging-lazy", {logger.info {"This is logging of - kotlin-logging"}});
+        test("kotlin-logging-lazy", {logger.info {"This is logging of - kotlin-logging"}});
+        test("AnkoLogger", {info ("This is logging of - AnkoLogger" )});
+        test("AnkoLogger", {info ("This is logging of - AnkoLogger" )});
+        test("AnkoLogger", {info ("This is logging of - AnkoLogger" )});
+        test("AnkoLogger", {info ("This is logging of - AnkoLogger" )});
+        test("AnkoLogger", {info ("This is logging of - AnkoLogger" )});
+        test("AnkoLogger-lazy", {info {"This is logging of - AnkoLogger"}});
+        test("AnkoLogger-lazy", {info {"This is logging of - AnkoLogger"}});
+        test("AnkoLogger-lazy", {info {"This is logging of - AnkoLogger"}});
+        test("AnkoLogger-lazy", {info {"This is logging of - AnkoLogger"}});
+        test("AnkoLogger-lazy", {info {"This is logging of - AnkoLogger"}});
+    }
+
+    private fun test(testName: String, func: ()-> Unit): Unit {
+        val st = Stopwatch.createStarted()
+        for (i in 1..1000) {
+            func.invoke()
+        }
+        Log.e(TAG, "test-timing: '$testName' took ${st}")
+
     }
 
     companion object: KLogging()
