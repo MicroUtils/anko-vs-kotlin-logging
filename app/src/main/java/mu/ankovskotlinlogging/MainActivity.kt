52199e867dc91e639017c36b40a1d8b5f28d834b
oshai <ohadshai@gmail.com>
2016-08-11 23:40:33 +0300

    added anko logging

--- app/src/main/java/mu/ankovskotlinlogging/MainActivity.kt
+++ app/src/main/java/mu/ankovskotlinlogging/MainActivity.kt
@@ -4,14 +4,18 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import mu.KLogging
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.v(TAG, "This is logging of - android.util.Log logging");
         logger.info { "This is logging of - kotlin-logging" }
+        info("This is logging of - AnkoLogger")
+        info{"This is logging of - AnkoLogger2"}
     }
 
     companion object: KLogging()
